760a709fa6a99d6a1f3e2336910d5e9cb166c435
Cyrus Arora <cyrus.s.arora@gmail.com>
2019-02-25 22:22:12 -0800

    Got that error fixing

--- app/game.py
+++ app/game.py
@@ -1,4 +1,5 @@
 import networkx as nx
+from collections import OrderedDict
 
 
 class Game:
@@ -14,63 +15,59 @@ class Game:
         self.snakes = []
 
     def update_game(self, game_data):
+        self.head = (game_data["you"]["body"][0]["x"], game_data["you"]["body"][0]["y"])
+        self.tail = (game_data["you"]["body"][-1]["x"], game_data["you"]["body"][-1]["y"])
+        self.my_length = self.get_my_length(game_data["you"]["body"])
+        self.foods = [(food["x"], food["y"]) for food in game_data["board"]["food"]]
         self.update_snakes(game_data)
         self.update_board()
         self.health = game_data["you"]["health"]
-        self.head = Node(game_data["you"]["body"][0]["x"], game_data["you"]["body"][0]["y"])
-        self.tail = Node(game_data["you"]["body"][-1]["x"], game_data["you"]["body"][-1]["y"])
-        self.foods = [Node(food["x"], food["y"]) for food in game_data["board"]["food"]]
 
     def update_snakes(self, game_data):
         for snake in game_data["board"]["snakes"]:
-            self.snakes = [Node(point["x"], point["y"]) for point in snake["body"]]
-        self.snakes.remove(Node(game_data["you"]["body"][0]["x"], game_data["you"]["body"][0]["y"]))
-        self.snakes.remove(Node(game_data["you"]["body"][-1]["x"], game_data["you"]["body"][-1]["y"]))
+            self.snakes = [(point["x"], point["y"]) for point in snake["body"]]
+        self.snakes = list(OrderedDict.fromkeys(self.snakes))
+        self.snakes.remove((game_data["you"]["body"][0]["x"], game_data["you"]["body"][0]["y"]))
+        if len(game_data["you"]["body"]) > 1 and len(self.snakes)>0:
+            self.snakes.remove((game_data["you"]["body"][-1]["x"], game_data["you"]["body"][-1]["y"]))
 
     def update_board(self):
         self.board = nx.Graph()
         for y in range(self.board_height):
             for x in range(self.board_width):
-                current_node = Node(x, y)
+                current_node = (x, y)
                 if current_node in self.snakes:
                     continue
-                self.board.add_node(current_node.string())
+                self.board.add_node(current_node)
                 self.add_edges(current_node)
 
     def add_edges(self, current_node):
-        x = current_node.x
-        y = current_node.y
-        for node in [Node(x-1, y), Node(x+1, y), Node(x, y-1), Node(x, y+1)]:
-            if self.board.has_node(node.string()):
-                self.board.add_edge(node.string(), current_node.string())
+        x = current_node[0]
+        y = current_node[1]
+        for node in [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]:
+            if self.board.has_node(node) and ((current_node not in [self.head, self.tail]) or (node not in [self.head, self.tail])):
+                self.board.add_edge(node, current_node)
 
     def print_board(self):
         nx.draw_networkx(self.board)
 
-    def get_node(self, node_string):
-        return Node(int(node_string[0]), int(node_string[1]))
+    def make_node(self, x, y):
+        return str(x) + str(y)
 
     def get_move(self):
-        destination = nx.shortest_path(self.board, self.head.string(), self.tail.string())[0]
-        return self.get_direction(self.get_node(destination))
+        if self.my_length == 1:
+            return 'up'
+        destination = nx.shortest_path(self.board, self.head, self.tail)[1]
+        return self.get_direction(destination)
 
     def get_direction(self, destination):
-        if self.head.x == destination.x:
-            if self.head.y > destination.y:
-                return "down"
-            return "up"
-        if self.head.x > destination.x:
-            return "left"
-        return "right"
+        if self.head[0] == destination[0]:
+            if self.head[1] > destination[1]:
+                return 'down'
+            return 'up'
+        if self.head[0] > destination[0]:
+            return 'left'
+        return 'right'
 
-
-class Node:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def string(self):
-        return str(self.x) + str(self.y)
-
-    def __eq__(self, other):
-        return self.x == other.x and self.y == other.y
+    def get_my_length(self, my_body):
+        return len(list(OrderedDict.fromkeys([self.make_node(point["x"], point["y"]) for point in my_body])))
